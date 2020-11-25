b67705443e93189302da23cb203f39e1bfb56316
MAKSIM-BRY <maxim.barry@gmail.com>
2020-11-25 16:22:00 +0100

    Ajout recommendation.

--- src/views/ResultPage.vue
+++ src/views/ResultPage.vue
@@ -15,6 +15,8 @@
       <div class="paragraphe" >
         Nous vous informons que votre test c'est avéré <span class="negatif">positif</span>, votre médecin traitan {{ nomMedecinTraitant }} et l'assurance maladie ont été informé.
       </div>
+      <br/>
+      <div class="paragraphe">Un mail à été envoyé à votre adress électronique: <span>{{adresseMail}}</span></div>
       <button class="printButton" v-on:click="printPDF">Imprimer</button>
     </div>
     <div v-if="resultCovid === false" >
@@ -29,6 +31,54 @@
       <div class="paragraphe">Un mail à été envoyé à votre adress électronique: <span>{{adresseMail}}</span></div>
       <button class="printButton" v-on:click="printPDF">Imprimer</button>
     </div>
+    <div class="paragraphe recommandation">
+        <span>Recommandation:</span>
+        <br/><br/>
+        - Se laver régulièrement les mains ou utiliser une solution hydro-alcoolique
+        <br/>
+        - Tousser ou éternuer dans son coude ou dans son mouchoir
+        <br />
+        - Se moucher dans un mouchoir à usage unique puis le jeter
+        <br />
+        - Porter un masque quand la distance d’un mètre ne peut pas être respectée et partout où cela est obligatoire
+        <br />
+        - Respecter une distance d’au moins un mètre avec les autres
+        <br />
+        - Limiter au maximum ses contacts sociaux (6 maximum)
+        <br />
+        - Éviter de se toucher le visage
+        <br />
+        - Se moucher dans un mouchoir à usage unique puis le jeter
+        <br />
+        - Aérer les pièces 10 minutes, trois fois par jour
+        <br />
+        - Saluer sans serrer la main et arrêter les embrassades
+        <br />
+        - Utiliser les outils numériques (TousAntiCovid)
+        <!--<br />
+        <div>Se laver régulièrement les mains ou utiliser une solution hydro-alcoolique</div>
+        <br />
+        <div>Tousser ou éternuer dans son coude ou dans son mouchoir</div>
+        <br />
+        <div>Se moucher dans un mouchoir à usage unique puis le jeter</div>
+        <br />
+        <div>Porter un masque quand la distance d’un mètre ne peut pas être respectée et partout où cela est obligatoire</div>
+        <br />
+        <div>Respecter une distance d’au moins un mètre avec les autres</div>
+        <br />
+        <div>Limiter au maximum ses contacts sociaux (6 maximum)</div>
+        <br />
+        <div>Éviter de se toucher le visage</div>
+        <br />
+        <div>Se moucher dans un mouchoir à usage unique puis le jeter</div>
+        <br />
+        <div>Aérer les pièces 10 minutes, trois fois par jour</div>
+        <br />
+        <div>Saluer sans serrer la main et arrêter les embrassades</div>
+        <br />
+        <div>Utiliser les outils numériques (TousAntiCovid)</div>-->
+    </div>
+
   </div>
 </template>
 
@@ -115,6 +165,13 @@ export default {
     margin-right: 15%;
     margin-left: 15%;
     font-size: 30px;
+  }
+
+  .recommandation {
+    position: fixed;
+    bottom: 0px;
+    font-size: 18px;
+    margin-bottom: 10px;
   }
 
   span{
